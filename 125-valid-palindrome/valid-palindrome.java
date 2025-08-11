b6a5e3c1992aeb75eb566e9a8d8f3db7a002ddac
Vikas K <vikask050905@gmail.com>
2025-08-11 23:23:30 +0530

    Time: 3 ms (72.85%) | Memory: 43.2 MB (70.11%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase();
+        int left = 0, right = s.length() - 1;
+        
+        while (left < right) {
+            if (!Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            } else if (!Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+            else if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            } else {
+                left++;
+                right--;
+            }
+        }
+        return true;
+    }
+}
