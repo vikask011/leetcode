de9df65ae34b005ce9d5c57bcb29b99bc03280fd
Vikas K <vikask050905@gmail.com>
2026-06-09 12:48:19 +0530

    Time: 1 ms (99.67%) | Memory: 47.2 MB (73.82%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -1,22 +1,18 @@
 class Solution {
     public int jump(int[] nums) {
-       if(nums.length<=1){
-        return 0;
-       }
-       int jumps=0;
-       int far=0;
-       int curr=0;
+        int jumps=0;
+        int curend=0;
+        int far=0;
 
-       for(int i=0;i<nums.length-1;i++){
-        far=Math.max(far,i + nums[i] );
-       
+        for(int i=0;i<nums.length-1;i++){
+            far=Math.max(far , i+nums[i]);
 
-       if(i==curr){
-        jumps++;
+            if(i==curend){
+                jumps++;
+                curend=far;
+            }
+        }
+        return jumps;
         
-       
-       curr=far;
-       }}
-       return jumps;
     }
 }
